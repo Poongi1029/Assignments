d202b19a333ef0ac3bedbed29716cdd2047bcd6d
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 16:17:36 +0530

    Delete BankAccountService.java

--- SpringCore/main/java/SpringCoreQ4/BankAccountService.java
+++
@@ -1,8 +0,0 @@
-package springcoreq4;
-
-public interface BankAccountService {
-    public double withdraw(long accountId, double balance);
-    public double deposit(long accountId, double balance);
-    public double getBalance(long accountId);
-    public boolean fundTransfer(long fromAccount, long toAccount, double amont);
-}
