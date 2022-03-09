1083bface8d2f8934ee08fd5598bf19c585f6004
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 15:50:46 +0530

    SpringCore

---
+++ SpringCore/main/java/SpringCoreQ3/BankAccountRepository.java
@@ -0,0 +1,6 @@
+package springcoreq3;
+
+public interface BankAccountRepository {
+    public double getBalance(long accountId);
+    public double updateBalance(long accountId, double newBalance);
+}
