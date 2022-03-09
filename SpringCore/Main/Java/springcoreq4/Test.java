45cefd7743ee6fad45d831733e0888830855e161
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 16:26:53 +0530

    SpringCore

--- SpringCore/Main/Java/springcoreq4/Test.java
+++ SpringCore/Main/Java/springcoreq4/Test.java
@@ -1,1 +1,25 @@
+package springcoreq4;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("springq4.xml");
+
+        BankAccountController controller = (BankAccountController)context.getBean("controller");
+
+
+        System.out.println(controller.getBalance(1171));
+        System.out.println(controller.deposit(1171,5000));
+        System.out.println("+++++++++++++++++++++++++++");
+        System.out.println(controller.withdraw(1171,5000));
+        System.out.println(controller.getBalance(1171));
+        System.out.println("___________________________________________");
+        System.out.println(controller.fundTransfer(1171,1172,5000));
+        System.out.println(controller.getBalance(1171));
+        System.out.println(controller.getBalance(1172));
+
+
+    }
+}
