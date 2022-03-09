d8b00aecf6de4ceefa185cec18b0daa077838ff9
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 16:24:32 +0530

    SpringCore

--- SpringCore/Main/Java/springcoreq2/Test.java
+++ SpringCore/Main/Java/springcoreq2/Test.java
@@ -1,1 +1,27 @@
+package springcoreq2;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("springq2.xml");
+
+        // calling list of answer
+       Question question =(Question)context.getBean("Question");
+       question.displayAnswerList();
+
+
+       // calling set of answer
+        System.out.println("=====================================");
+        Question question1 =(Question)context.getBean("Question1");
+        question1.displayAnswerSet();
+
+        // calling map of answer
+        System.out.println("=====================================");
+        Question question2 =(Question)context.getBean("Question2");
+        question2.displayAnswerMap();
+
+    }
+}
