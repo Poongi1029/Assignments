684c556b5f7e076b3697ac81564b2b8ad31b080a
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 16:05:31 +0530

    Delete BankAccountRepository.java

--- SpringCore/main/java/SpringCoreQ4/BankAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package springcoreq4;
-
-public interface BankAccountRepository {
-    public double getBalance(long accountId);
-    public double updateBalance(long accountId, double newBalance);
-}
