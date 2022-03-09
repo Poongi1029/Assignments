f2a598d8fad10af2f3c6c4b570ee1f7b7f6e9fe3
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-09 16:07:02 +0530

    SpringCore

--- SpringCore/main/java/SpringCoreQ4/Test.java
+++ SpringCore/main/java/SpringCoreQ4/Test.java
@@ -15,7 +15,7 @@ public class Test {
         System.out.println("+++++++++++++++++++++++++++");
         System.out.println(controller.withdraw(1171,5000));
         System.out.println(controller.getBalance(1171));
-        System.out.println("___________________________________________");
+        System.out.println("__________________________________________");
         System.out.println(controller.fundTransfer(1171,1172,5000));
         System.out.println(controller.getBalance(1171));
         System.out.println(controller.getBalance(1172));
