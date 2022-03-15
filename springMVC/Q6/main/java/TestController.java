fa6070b46219ae8da057aa376f1d5f1ae694cc16
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-15 22:43:00 +0530

    springmvc

---
+++ springMVC/Q6/main/java/TestController.java
@@ -0,0 +1,32 @@
+package com.Q6;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	
+	 @RequestMapping(value = "/registration", method = RequestMethod.GET)
+	    public ModelAndView registrationForm() {
+	        ModelAndView modelAndView = new ModelAndView("registration");
+	        return modelAndView;
+
+	    }
+	    @RequestMapping(value = "/validation", method = RequestMethod.POST)
+	    public ModelAndView formValidation(@Valid @ModelAttribute("customer")Customer customer1, BindingResult bindingResult) {
+	        if(bindingResult.hasErrors()) {
+	            System.out.println("inside");
+	            ModelAndView modelAndView = new ModelAndView("registration");
+	            return modelAndView;
+	        }
+	        ModelAndView modelAndView = new ModelAndView("test");
+	        modelAndView.addObject("msg",customer1);
+	        return modelAndView;
+	    }
+}
