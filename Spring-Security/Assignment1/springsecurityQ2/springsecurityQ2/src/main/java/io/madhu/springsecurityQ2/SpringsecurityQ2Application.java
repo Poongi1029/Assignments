2fb1f6200f5aaf29167b0efd28064ab3544e607f
Poonguzhali <99066953+Poongi1029@users.noreply.github.com>
2022-03-29 13:45:34 +0530

    springsecurity

---
+++ Spring-Security/Assignment1/springsecurityQ2/springsecurityQ2/src/main/java/io/madhu/springsecurityQ2/SpringsecurityQ2Application.java
@@ -0,0 +1,13 @@
+package io.madhu.springsecurityQ2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityQ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityQ2Application.class, args);
+	}
+
+}
---
+++ Spring-Security/Assignment1/springsecurityQ2/springsecurityQ2/src/test/java/io/madhu/springsecurityQ2/SpringsecurityQ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.madhu.springsecurityQ2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityQ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
